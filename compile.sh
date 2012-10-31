58111e8eee887f74a2efbd0d12b37a97a8e39ccf
Rickard Lindberg <ricli85@gmail.com>
2012-10-31 16:22:58 +0100

    edit

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mkdir -p html-export
+rm -r html-export
+mkdir html-export
 cp -r static html-export
 cd ~/projects/markdown-wiki/
 ./run ~/projects/compiler-tutorial/main ~/projects/compiler-tutorial/html-export
