f49e7e4d9c59ca36c1617c9cbccabe5039cedeb7
Rickard Lindberg <ricli85@gmail.com>
2012-11-01 10:33:51 +0100

    edit

---
+++ src/ParseTree.hs
@@ -0,0 +1,9 @@
+module ParseTree where
+
+data Program = Program [Let]
+
+data Let     = Let String Term
+
+data Term    = Identifier String
+             | Number     Integer
+             | Lambda     [Let] [String] [Term]
