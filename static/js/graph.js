d0cb885b8c0eea1444030bb6706a3d7ffb5f50de
Rickard Lindberg <ricli85@gmail.com>
2012-11-01 16:57:20 +0100

    publishing e4d373f add template

--- static/js/graph.js
+++ static/js/graph.js
@@ -17,13 +17,13 @@ function Graph( canvas_name, width, height ) {
 	this.spring_length = 20; // base resting length of springs
 }
 
-Graph.prototype.createVertex = function( name, color, stroke, clickFn ) { // XXX -- should support separate id and name 
+Graph.prototype.createVertex = function( name, colors, clickFn ) { // XXX -- should support separate id and name 
 	// create an SVG rectangle, attach additional attributed to it
 	var vertex = document.createElementNS(this.svg, "rect");
-	if( color === undefined ) {
-		color = "#222";
+	if( colors === undefined ) {
+		colors = { color: "#222", stroke: "#000" };
 	}
-	vertex.setAttribute("style", "fill: "+color+"; stroke: "+stroke+"; stroke-width: 1px;");
+	vertex.setAttribute("style", "fill: "+colors.color+"; stroke: "+colors.stroke+"; stroke-width: 1px;");
 	vertex.setAttribute("rx", "10px"); // round the edges
 	// random placement with a 10% margin at the edges
 	vertex.posx = Math.random() * (this.width * 0.8) + (this.width * 0.1);
